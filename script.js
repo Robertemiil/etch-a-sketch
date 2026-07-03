b38b3af1b8132b805e83c087b57dd274a3c98ca6
robertemiil <144625355+Robertemiil@users.noreply.github.com>
2026-07-03 11:51:54 -0400

    feat: improve cell quantity handling and enhance layout with navbar adjustments

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
-
-
+const header = document.createElement('header');
+document.body.prepend(header);
+header.textContent= 'Etch-A-Sketch';
+const navBar = document.createElement('nav');
+header.after(navBar);
+navBar.classList.add('navBar');
 const container = document.createElement('div');
 container.classList.add('container');
-document.body.appendChild(container);
+navBar.after(container);
 const qtyButton = document.createElement('button');
 qtyButton.classList.add('qtyButton');
 qtyButton.textContent='click me';
-document.body.prepend(qtyButton);
-let cells  = 0;
+navBar.prepend(qtyButton);
+let cells  ;
 const cellsLimit = 25;
 const resetBtn = document.createElement('button');
 resetBtn.textContent ='reset';
-
-
+navBar.appendChild(resetBtn);
 
 
 const cellQty = ()=>{
+  reset(container);
   const qty = Number(prompt('how many cells do you need ?'));
-   if (!Number.isInteger(qty) || qty <= 0) {
+  qtyButton.addEventListener('click',()=>{
+    if(qty <= cellsLimit)return qty;
+
+    else if(qty >= cellsLimit) 
+    return alert('cells exceeded the limits , please try a number below 25');
+
+    if(!Number.isInteger(qty) || qty <= 0) {
      alert('Please enter a valid positive number.');
      return;
-   }
+    }
 
+  }
+   );
+  
+   //make cells squared
    const totalCells = cells + qty;
    container.style.gridTemplateColumns = `repeat(${qty}, 1fr)`;
    container.style.gridTemplateRows = `repeat(${qty}, 1fr)`;
@@ -31,28 +45,21 @@ const cellQty = ()=>{
      cell.classList.add(`div${i}`, 'grid-cell');
      container.appendChild(cell);
     }
-  
-   
+   //For Test purposes
    cells = totalCells;
    console.log(cells);
-
+  //conditional statement avoid event 
+  //from happening on blank container 
   if(cells!=0){
   container.addEventListener('mouseover', (e)=>{
   const target = e.target;
+  if(target.classList.contains('grid-cell'))
   target.style.backgroundColor = randomColor();
   });
  };
 }
 
-
-qtyButton.addEventListener('click',()=>{
-     if(cells <= cellsLimit)return cellQty();
-     else if(cells >= cellsLimit) 
-     return alert('cells exceeded the limits , please reset');
- }
-);
-
-
+qtyButton.addEventListener('click',()=>cellQty());
 
 function randomColor(){
   let result;
@@ -63,5 +70,13 @@ function randomColor(){
 
   return  result = `rgba(${red},${green},${blue},${alpha})`; 
 };
-  
+
+const reset = (r) =>{
+  r.innerHTML='';
+  cells = 0;
+   
+};
+resetBtn.addEventListener('click', ()=>{
+  reset(container);
+});
 
